9c11c9862e3f570dc61ad16d5a644cc64008f4d1
vijit-vishnoi <vishnoivijit@gamil.com>
2025-01-25 22:08:57 +0530

    Added notification ticket model

---
+++ src/models/notificationticket.js
@@ -0,0 +1,43 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class NotificationTicket extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  NotificationTicket.init({
+    subject:{
+      type: DataTypes.STRING,
+      allwoNull: false,
+    },
+    content:{
+      type: DataTypes.STRING,
+      allwoNull: false,
+    } ,
+    recepientEmail:{
+      type: DataTypes.STRING,
+      allwoNull: false,
+    },
+    status:{
+      type: DataTypes.STRING,
+      allwoNull: false,
+      values:["PENDING","SUCCESS","FAILED"]
+    },
+    notificationTime:{
+      type: DataTypes.DATE,
+      allowNull: false
+    } 
+  }, {
+    sequelize,
+    modelName: 'NotificationTicket',
+  });
+  return NotificationTicket;
+};
